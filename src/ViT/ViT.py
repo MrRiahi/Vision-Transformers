16ab99bb9af036190416282017b1e2d2f4f4fac4
MrRiahi <mohammad.r.riahi@gmail.com>
2022-09-23 19:29:02 +0330

    Build GeneratePatch layer

--- src/ViT/ViT.py
+++ src/ViT/ViT.py
@@ -1,4 +1,6 @@
+from tensorflow.keras.layers import Input
 from tensorflow.keras.layers import Layer
+import tensorflow as tf
 
 
 class GeneratePatches(Layer):
@@ -6,11 +8,29 @@ class GeneratePatches(Layer):
     A layer for generating the image patches.
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, patch):
+        super(GeneratePatches, self).__init__()
+        self.patch = patch
 
-    def call(self, inputs, **kwargs):
-        pass
+    def call(self, images):
+        """
+        Generate the images patch and flatten them
+        :param images: batch of images
+        :return:
+        """
+
+        images_patches = tf.image.extract_patches(images=images,
+                                                  sizes=[1, self.patch, self.patch, 1],
+                                                  strides=[1, self.patch, self.patch, 1],
+                                                  rates=[1, 1, 1, 1],
+                                                  padding='valid')
+
+        batch_size = tf.shape(images)[0]
+        patch_dimension = tf.shape(images_patches)[-1]
+
+        patches = tf.reshape(images_patches, [batch_size, -1, patch_dimension])
+
+        return patches
 
 
 class ViT:
@@ -18,13 +38,18 @@ class ViT:
     Implementation of the ViT model for image classification.
     """
 
-    def __init__(self, classes):
+    def __init__(self, classes, patch=1):
         self.classes = classes
+        self.patch = patch
 
-    def __call__(self):
+        self.generate_patches = GeneratePatches(patch=self.patch)
+
+    def __call__(self, X):
         """
         Implement the ViT model
         :return:
         """
 
-        pass
+        # Define the input as a tensor with shape input_shape
+        X_input = Input((self.input_shape[0], self.input_shape[1], 3))
+
