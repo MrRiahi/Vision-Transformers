21cae24ae85d65555ee6fd51a7ecdedb5c002a03
riahi <mohammad.r.riahi@gmail.com>
2022-12-18 10:08:37 +0330

    Update train module

--- train.py
+++ train.py
@@ -13,27 +13,27 @@ model, input_shape = get_model(classes_numbers=Cfg.CIFAR_10_CLASS_NUMBERS)
 train_dataset, val_dataset = get_train_dataset(input_shape=input_shape, color_mode='rgb')
 
 # Use ModelCheckpoint to control validation loss for saving the best model.
-best_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=Cfg.MODEL_PATH + '/best.val_loss_{val_loss:.2f}',
-                                                              monitor='val_loss',
-                                                              verbose=1,
-                                                              save_best_only=True)
+# best_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=Cfg.MODEL_PATH + '/best.val_loss_{val_loss:.2f}',
+#                                                               monitor='val_loss',
+#                                                               verbose=1,
+#                                                               save_best_only=True)
 
 # Use ModelCheckpoint to save last checkpoint
-last_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=f'{Cfg.MODEL_PATH}/last',
-                                                              verbose=1,
-                                                              save_freq='epoch')
+# last_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=f'{Cfg.MODEL_PATH}/last',
+#                                                               verbose=1,
+#                                                               save_freq='epoch')
 
 # Use LearningRateScheduler to decrease the learning rate during training.
-learning_rate = tf.keras.callbacks.LearningRateScheduler(UtilityFunction.learning_rate_decay)
-
-callbacks = [best_checkpoint_callback, last_checkpoint_callback, learning_rate]
+# learning_rate = tf.keras.callbacks.LearningRateScheduler(UtilityFunction.learning_rate_decay)
+#
+# callbacks = [last_checkpoint_callback, learning_rate]
 
 # Train network
-history = model.fit(train_dataset, validation_data=val_dataset,
-                    epochs=Cfg.EPOCHS, callbacks=callbacks)
 # history = model.fit(train_dataset, validation_data=val_dataset,
-#                     epochs=Cfg.EPOCHS)
+#                     epochs=Cfg.EPOCHS, callbacks=callbacks)
+history = model.fit(train_dataset, validation_data=val_dataset,
+                    epochs=Cfg.EPOCHS)
 
 # Save history
 # np.save(f'{model_path}/history.npy', history.history)
-UtilityFunction.save_history(history=history.history)
+# UtilityFunction.save_history(history=history.history)
